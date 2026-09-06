c0537f678794b826ce9bbccc5b49c13029500d6c
qaCow37 <daily.cow37@proton.me>
2026-09-06 16:20:12 +0000

    removed devshell for prismgen

--- flake.nix
+++ flake.nix
@@ -1,75 +1,58 @@
 {
     description = "Flake to install PrismLauncher for Minecraft and create your instances declaratively";
 
-	inputs = {
-		prismlauncher = {
-			url = "github:PrismLauncher/PrismLauncher";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		home-manager =  {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-	};
-	outputs = {self, nixpkgs, prismlauncher, home-manager, flake-utils, ...}@inputs:
-	let
+    inputs = {
+        prismlauncher = {
+            url = "github:PrismLauncher/PrismLauncher";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        home-manager =  {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+    outputs = {self, nixpkgs, prismlauncher, home-manager, flake-utils, ...}@inputs:
+    let
         systems = builtins.attrNames (prismlauncher.packages);
-		lib = nixpkgs.lib // home-manager.lib // self.lib;
-	in
-	{
-		homeModules = rec {
-			prismnixWith = {...}@args: (
-				import ./homeModules/prismnix.nix ({
-					lib = lib;
-					inputs = inputs;
-				} // args)
-			);
-			prismnix = prismnixWith {};
-		};
-		lib.prismnix = import ./lib {inherit lib;};
+        lib = nixpkgs.lib // home-manager.lib // self.lib;
+    in
+    {
+        homeModules = rec {
+            prismnixWith = {...}@args: (
+                import ./homeModules/prismnix.nix ({
+                    lib = lib;
+                    inputs = inputs;
+                } // args)
+            );
+            prismnix = prismnixWith {};
+        };
+        lib.prismnix = import ./lib {inherit lib;};
 
-		overlays = {
-			default = import ./overlays/default.nix {
+        overlays = {
+            default = import ./overlays/default.nix {
                 lib = lib;
                 inputs = inputs;
             };
-		};
-	} // flake-utils.lib.eachSystem systems (system:
-		let
-			pkgs = nixpkgs.legacyPackages.${system};
-		in
-		{
+        };
+    } // flake-utils.lib.eachSystem systems (system:
+        let
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
             docs = import ./docs {
                 lib = lib;
                 inputs = inputs;
                 pkgs = pkgs;
             };
 
-			packages = import ./packages {
-				lib = lib;
-				pkgs = pkgs;
-				inputs = inputs;
-				system = system;
-			};
-
-			devShells.default = pkgs.mkShell {
-				#
-				# Build CLI with
-				# pyhthon dependencies
-				#
-				packages = with pkgs; [
-					python3
-					python3Packages.requests
-					python3Packages.packaging
-					python3Packages.requests-ratelimiter
-					python3Packages.typer
-					python3Packages.jinja2
-
-                    pyright
-				];
-			};
-		}
-	);
+            packages = import ./packages {
+                lib = lib;
+                pkgs = pkgs;
+                inputs = inputs;
+                system = system;
+            };
+        }
+    );
 }
